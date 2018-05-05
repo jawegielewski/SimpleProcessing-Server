55fd9ec6d3104d88e95ad5624ab5887a5d51a7a3
Kuba <jawegielewski@gmail.com>
2018-05-05 20:46:19 +0200

    ch

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -1,3 +1,7 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -36,6 +40,17 @@ public class MyServlet extends HttpServlet {
         writer.write(doubledValue.toString());
         writer.flush();
         writer.close();
+    }
+
+
+    public static void main(String[] args) throws Exception{
+        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(new MyServlet()),"/*");
+        server.start();
+        server.join();
     }
 }
 
